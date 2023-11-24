ef8ca7b1bfa2e9c4f49530cbce930517dc3a8f31
Muhammad Rifa <rifamaulana243@gmail.com>
2023-11-24 22:25:56 +0700

    tesfont1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        Lato: ["Lato"],
+        Lato: ["Lato", "sans-serif"],
+        Poppins: ["Poppins", "sans-serif"],
       },
     },
   },
