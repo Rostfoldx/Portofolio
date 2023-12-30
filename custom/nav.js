efec627340994cd9fa78a50d6d22161bc7215bd1
Muhammad Rifa <rifamaulana243@gmail.com>
2023-12-30 22:03:37 +0700

    -a

--- custom/nav.js
+++ custom/nav.js
@@ -1,18 +1,18 @@
-let prevScrollPos = window.scrollY;
-const lgNav = document.querySelectorAll(".lgNav");
-
 window.addEventListener("scroll", () => {
-  const currentScrollPos = window.scrollY;
+  const Scroll = window.scrollY;
+  const lgNav = document.querySelectorAll(".menu li");
 
-  if (prevScrollPos < currentScrollPos) {
+  if (Scroll > 1) {
     document.querySelector("#nav").classList.add("show");
     document.querySelector("#btnNav").classList.add("show2");
-    for (let i = 0; i < lgNav.length; i++) {
-      lgNav[i].classList.add("show2");
-    }
+    lgNav.forEach((item) => {
+      item.style.color = white;
+    });
   } else {
     document.querySelector("#nav").classList.remove("show");
     document.querySelector("#btnNav").classList.remove("show2");
-    lgNav[i].classList.add("show3");
+    lgNav.forEach((item) => {
+      item.style.color = black;
+    });
   }
 });
