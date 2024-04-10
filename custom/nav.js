b295ee1846b6963c5b782fbc4af1a95050c4f704
Muhammad Rifa <rifamaulana243@gmail.com>
2024-04-10 19:12:41 +0700

    fix the color

--- custom/nav.js
+++ custom/nav.js
@@ -1,18 +1,19 @@
 window.addEventListener("scroll", () => {
   const Scroll = window.scrollY;
-  const lgNav = document.querySelectorAll(".menu li");
+  const lgNav = document.querySelectorAll(".menu a");
 
   if (Scroll > 1) {
     document.querySelector("#nav").classList.add("show");
     document.querySelector("#btnNav").classList.add("show2");
-    lgNav.forEach((item) => {
-      item.style.color = white;
+
+    lgNav.forEach(function (item) {
+      item.style.color = "white";
     });
   } else {
+    lgNav.forEach(function (item) {
+      item.style.color = "black";
+    });
     document.querySelector("#nav").classList.remove("show");
     document.querySelector("#btnNav").classList.remove("show2");
-    lgNav.forEach((item) => {
-      item.style.color = black;
-    });
   }
 });
